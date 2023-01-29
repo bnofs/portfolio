2b2638732dc43959357942876859c2c898bf2b6f
nobr8001 <brendan.nofs@nielsen.com>
2023-01-29 15:37:14 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Design Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
