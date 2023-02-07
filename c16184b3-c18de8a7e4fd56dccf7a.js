abb158d760a5557717edb588a633252a861198d7
nobr8001 <brendan.nofs@nielsen.com>
2023-02-06 19:53:27 -0700

    Updates

---
+++ c16184b3-c18de8a7e4fd56dccf7a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdesign_portfolio=self.webpackChunkdesign_portfolio||[]).push([[976],{9417:function(c,n,i){i.d(n,{xiG:function(){return s}});var s={prefix:"fas",iconName:"bars",icon:[448,512,["navicon"],"f0c9","M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"]}}}]);
+//# sourceMappingURL=c16184b3-c18de8a7e4fd56dccf7a.js.map
